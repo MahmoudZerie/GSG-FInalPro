2a3b2127c6f2d365f591cc5ca3bd381933c87e94
MahmoudZerie <mahmoudzeriemahmoud@gmail.com>
2024-10-27 20:19:29 +0200

    blogs section

--- script.js
+++ script.js
@@ -111,3 +111,23 @@ var swiper = new Swiper(".reviews-slider", {
 	  },
 	},
   });
+var swiper = new Swiper(".blogs-slider", {
+	loop:true,
+	spaceBetween:10,
+	centeredSlides:true,
+	autoplay:{
+		delay:9500,
+		disableOnInteraction:false,
+	},
+	breakpoints: {
+	 0: {
+		slidesPerView: 1,
+	  },
+	  768: {
+		slidesPerView: 2,
+	  },
+	  1024: {
+		slidesPerView: 3,
+	  },
+	},
+  });
