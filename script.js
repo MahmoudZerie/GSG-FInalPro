6d8b7f10112f02c74000b73fadd87ab795876306
MahmoudZerie <mahmoudzeriemahmoud@gmail.com>
2024-10-27 19:12:22 +0200

    review section

--- script.js
+++ script.js
@@ -91,3 +91,23 @@ var swiper = new Swiper(".arrivals-slider", {
 	  },
 	},
   });
+var swiper = new Swiper(".reviews-slider", {
+	loop:true,
+	spaceBetween:10,
+	centeredSlides:true,
+	autoplay:{
+		delay:9500,
+		disableOnInteraction:false,
+	},
+	breakpoints: {
+	 0: {
+		slidesPerView: 1,
+	  },
+	  768: {
+		slidesPerView: 2,
+	  },
+	  1024: {
+		slidesPerView: 3,
+	  },
+	},
+  });
